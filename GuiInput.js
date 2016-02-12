3b7158db1f18d5d0be387e61d6364a961dbfbe7f
GuilhermeRossato <guilhermrossato@gmail.com>
2016-02-12 15:37:36 -0200

    Gui Input Main Class This huge class proved to be a real pain to create, lots of small details an input has to process.
    Four hundred lines for an Input UI simulation in canvas. This obviously counts as "reinventing the wheel", which is looked down upon, but it's *absolutely* perfect for numeric inputs, e-mails and whatnot, the only exception is that it is missing copy, paste and cut, that just couldn't be implemented. Also, the graphical part cannot be interpreted differently between browsers.
    
    Anyways, this is an experiment, I can afford to put my time in learning how silly things like inputs works, so this TextInput in Canvas taught me a lot of cool things about how things work.
    
    Some small tricks here and there, need to put a bit of work on undo/redo, but overall I'm very happy with this class!
    
    Worst con of using this virtualization of Text Input: *Absolutely no support for mobile devices*. Tough luck, not my applications main focus thought, so I'll set that aside.

--- GuiInput.js
+++ GuiInput.js
@@ -13,75 +13,351 @@ if (typeof(defaultSet)!=="function") {
 	defaultSet = (value,defaultValue) => (typeof(value) !== "number" || isNaN(value) || value == 0 )?defaultValue:value;
 }
 
-function GuiInput(x, y, width, height, numbersOnly) {
+function GuiInput(x, y, width, height, isNumeric, limit) {
+	var local_focus = false;
+	var local_text = "";
 	this.box = new GuiBox(x, y);
-	this.box.width = defaultSet(width,20)+4;
-	this.box.height = defaultSet(height,20)+4;
-	
-	if (ctx instanceof CanvasRenderingContext2D) {
-		ctx.font = this.graphic.font;
-		this.box.width = this.graphic.margin.left+this.graphic.margin.right+ctx.measureText(this.text).width;
-	} else
-		this.box.width = defaultSet(width+this.graphic.shadow,this.graphic.margin.left+this.graphic.margin.right+80);
-	this.box.height = defaultSet(height,20)+this.graphic.shadow;
-	Object.defineProperty(this,"boundingRect",{
+	this.box.width = defaultSet(width, 20);
+	this.box.height = defaultSet(height, 20);
+	this.limit = defaultSet(limit, 0);
+	Object.defineProperty(this,"text",{
 		configurable: false,
 		enumerable: false,
-		get: function() { return this.box; },
-		set: function(value) { console.error("boundingRect is unassignable"); }
-	});
-	Object.defineProperty(this,"isToggle",{
-		configurable: false,
-		enumerable: false,
-		get: function() { return (state >= 3) },
+		get: function() { return local_text; },
 		set: function(value) {
-			if (value && (state < 3))
-				state += 4;
-			else if ((!value) && (state >= 3))
-				state -= 4;
+			if ((value !== local_text) && ((this.limit < 1) || ((typeof(value)=== "string") && (value.length < this.limit)))) {
+				if (this.pastText.length > 10)
+					this.pastText.shift();
+				this.pastText.push(local_text);
+				this.futureText = [];
+				return (local_text = value);
+			}
+			return false;
 		}
 	});
+	
+	
+	this.undo = function() {
+		if (this instanceof GuiInput) {
+			if (this.pastText.length > 0) {
+				local_text = this.pastText.pop();
+				this.futureText.push(local_text);
+			}
+		}
+	}
+	
+	this.redo = function() {
+		if (this instanceof GuiInput) {
+			if (this.futureText.length > 0) {
+				local_text = this.futureText.shift();
+			}
+		}
+	}
+	
+	Object.defineProperty(this,"focus",{
+		configurable: false,
+		enumerable: false,
+		get: function() { return local_focus; },
+		set: function(value) {
+			if (local_focus !== value) {
+				if (value) {
+					local_focus = (this.blinkState = true);
+					this.startTimer();
+					this.forceDraw();
+				} else {
+					local_focus = (this.blinkState = false);
+					this.clearTimer();
+					this.forceDraw();
+				}
+			}
+			return value;
+		}
+	});
+	this.numeric = (isNumeric)?true:false;
 }
 
 GuiInput.prototype = {
 	constructor: GuiInput,
-	focus: false,
 	numeric: false,
 	multiLine: false,
-	graphic: {roundness: 2, focusBorder: 3, bgColor:"#33495E", focusColor:"#2E4154", textColor:"#EEE", font:"13px Arial", marginTop:5},
-	onMouseDown: function (btnId, x, y) {
-		if ((btnId === 0) && (this.state === 1) && this.box.checkBounds(x, y)) {
-			this.state = 2;
-			this.isDown = true;
-			if (this.onClickDown instanceof Function)
-				this.onClickDown(x-this.box.left, y-this.box.top);
+	selectStart: 0,
+	selectEnd: 0,
+	blinkState: false,
+	acceptsTab: true,
+	acceptsMail: true,
+	limit:0,
+	pastText: [],
+	futureText: [],
+	graphic: {roundness: 4, focusBorder: 2.5, bgColor:"#070E17", focusColor:"#006FFF", textColor:"#EEE", font:"13px Arial", margin:5, blinks: true},
+	startTimer: function() {
+		this.clearTimer();
+		this.blinkState = true;
+		var holdThis = this;
+		this.timer = setInterval(function() { holdThis.update.call(holdThis); }, 500);
+	},
+	clearTimer: function() {
+		if (typeof(this.timer) === "number") {
+			clearTimeout(this.timer);
+			this.timer = undefined;
+		}			
+	},
+	forceDraw: function() {
+		if (typeof(CanvasController) !== "undefined" && this.parent instanceof CanvasController) {
+			this.clear(this.parent.ctx);
+			this.draw(this.parent.ctx);
 		}
+	},
+	onMouseDown: function (btnId, x, y) {
+		if (typeof(CanvasController) !== "undefined" && this.parent instanceof CanvasController) {
+			if (this.box.checkBounds(x, y)) {
+				if (btnId === 0) {
+					var ctx = this.parent.ctx, leftNow = 0, i;
+					ctx.font = this.graphic.font;
+					for (i=0;i<this.text.length;i++)
+						if ((leftNow += ctx.measureText(this.text[i]).width)+this.graphic.margin > x-this.box.left+3)
+							break;
+					this.selectionStart = i;
+					this.selectionEnd = i;
+					this.focus = true;
+				} else if (btnId === 2) {
+					this.focus = true;
+				}
+			} else {
+				this.focus = false;
+			}
+			return true;
+		}
+		return false;
+	},
+	onKeyDown: function(keyCode, ctrlKey, shiftKey, altKey, event) { // Returns true if key was processed by this function
+		if (this instanceof GuiInput && this.focus) {
+			if (keyCode === 37) { // LEFT ARROW
+				var dest = this.selectionEnd - 1;
+				if (ctrlKey) {
+					while ((this.text[dest-1] !== "." && this.text[dest-1] !== " " && this.text[dest-1] !== "@")&&(dest > 1))
+						dest--;
+					if (dest === 1 && this.text[0] !== '.' && this.text[0] !== ' ') // Bug fixed by exception
+						dest--;
+				}
+				if (shiftKey) {
+					this.selectionEnd = Math.max(0, dest);
+				} else {
+					if (this.selectionStart !== this.selectionEnd) {
+						if (this.selectionEnd > this.selectionStart)
+							this.selectionEnd = this.selectionStart;
+						else
+							this.selectionStart = this.selectionEnd;
+					} else {
+						this.selectionStart = (this.selectionEnd = Math.max(0, dest));
+					}
+				}
+				this.startTimer();
+				return true;
+			} else if (keyCode === 39) { // RIGHT ARROW
+				var dest = this.selectionEnd + 1;
+				if (ctrlKey) {
+					while ((this.text[dest] !== "." && this.text[dest] !== " " && this.text[dest] !== "@")&&(dest < this.text.length))
+						dest++;
+					//if (dest !== this.text.length)
+						//dest--;
+				}
+				if (shiftKey) {
+					this.selectionEnd = Math.min(this.text.length, dest);
+				} else {
+					if (this.selectionStart !== this.selectionEnd) {
+						if (this.selectionEnd < this.selectionStart)
+							this.selectionEnd = this.selectionStart;
+						else
+							this.selectionStart = this.selectionEnd;
+					} else {
+						this.selectionStart = (this.selectionEnd = Math.min(this.text.length, dest));
+					}
+				}
+				this.startTimer();
+				return true;
+			} else if (keyCode === 36) { // HOME
+				if (shiftKey) {
+					this.selectionEnd = 0;
+				} else {
+					this.selectionStart = (this.selectionEnd = 0);
+				}
+				return true;
+			} else if (keyCode === 35) { // END
+				if (shiftKey) {
+					this.selectionEnd = this.text.length;
+				} else {
+					this.selectionStart = (this.selectionEnd = this.text.length);
+				}
+				return true;
+			} else if ((this.numeric) && (keyCode === 190 || keyCode === 194 || keyCode === 188 || keyCode === 110)) { // DOT / POINT / ZERO SEPARATOR
+				var i = this.text.indexOf('.');
+				var holdSelStart, holdSelEnd;
+				if (this.selectionEnd < this.selectionStart) {
+					holdSelStart = this.selectionEnd;
+					holdSelEnd = this.selectionStart;
+				} else {
+					holdSelStart = this.selectionStart;
+					holdSelEnd = this.selectionEnd;
+				}
+				if ((holdSelEnd !== i) && ((holdSelStart-i !== 1) || (holdSelStart !== holdSelEnd) || (this.text.length === 0))) {
+					this.startTimer();
+					if ((holdSelStart < i && holdSelEnd > i) || (i === -1)) {
+						this.text = this.text.substring(0,holdSelStart) + '.' + this.text.substring(holdSelEnd, this.text.length);
+						this.selectionStart = (this.selectionEnd = Math.min(holdSelStart+1,this.text.length));
+					} else if (holdSelEnd > i) {
+						this.text = this.text.replace('.', '');
+						this.text = this.text.substring(0,holdSelStart-1) + '.' + this.text.substring(holdSelEnd-1);
+						this.selectionStart = (this.selectionEnd = Math.min(holdSelStart,this.text.length));
+					} else if (holdSelEnd < i) {
+						this.text = this.text.replace('.', '');
+						this.text = this.text.substring(0,holdSelStart) + '.' + this.text.substring(holdSelEnd, this.text.length);
+						this.selectionStart = (this.selectionEnd = Math.min(holdSelStart+1,this.text.length));
+					}
+					if (this.onChange instanceof Function)
+						this.onChange.call(this);
+					return true;
+				}
+				return false
+			} else if (keyCode == 46) { // DELETE
+				var holdSelStart, holdSelEnd;
+				if (this.selectionEnd === this.selectionStart) {
+					//console.log("211 s",this.text.substring(0,this.selectionStart),this.text.substring(this.selectionStart+1));
+					this.text = this.text.substring(0,this.selectionStart) + this.text.substring(this.selectionStart+1);
+				} else if (this.selectionEnd < this.selectionStart) {
+					this.text = this.text.substring(0,this.selectionEnd) + this.text.substring(this.selectionStart);
+					this.selectionStart = this.selectionEnd;
+				} else {
+					this.text = this.text.substring(0,this.selectionStart) + this.text.substring(this.selectionEnd);
+					this.selectionEnd = this.selectionStart;
+				}
+				this.startTimer();
+				if (this.onChange instanceof Function)
+					this.onChange.call(this);
+				return true;
+			} else if (keyCode === 8) { // BACKSPACE
+				var holdSelStart, holdSelEnd;
+				if (this.selectionEnd === this.selectionStart) {
+					this.text = this.text.substring(0,this.selectionStart-1) + this.text.substring(this.selectionStart);
+					this.selectionStart = (this.selectionEnd = Math.max(0,this.selectionStart - 1))
+				} else if (this.selectionEnd < this.selectionStart) {
+					this.text = this.text.substring(0,this.selectionEnd) + this.text.substring(this.selectionStart);
+					this.selectionStart = this.selectionEnd;
+				} else {
+					this.text = this.text.substring(0,this.selectionStart) + this.text.substring(this.selectionEnd);
+					this.selectionEnd = this.selectionStart;
+				}
+				this.startTimer();
+				if (this.onChange instanceof Function)
+					this.onChange.call(this);
+				return true;
+			} else if (ctrlKey && keyCode === 86) {
+				console.log("redo");
+			} else if (ctrlKey && keyCode === 90) {
+				console.log("undo");
+			//} else if (ctrlKey && keyCode === 86) { // Ctrl + V (PASTE)
+				//console.log(event);
+				//return false;
+			} else if (ctrlKey && keyCode === 65) { // Ctrl + A (ALL)
+				this.selectionStart = 0;
+				this.selectionEnd = this.text.length;
+				return true;
+			} else if ((!ctrlKey)&&(((!this.numeric)&&((keyCode === 190)||(keyCode === 194)||(keyCode === 188)||(keyCode === 110)||(keyCode === 32)||(keyCode === 50 && shiftKey && this.acceptsMail)||(keyCode === 9 && this.acceptsTab)||(keyCode >= 65 && keyCode <= 90)||(keyCode >= 96 && keyCode <= 105)||(keyCode >= 48 && keyCode <= 57))) ||  // TAB, a ~ z, A ~ Z, 0 ~ 9
+			 	((this.numeric)&&((keyCode >= 96 && keyCode <= 105)||(keyCode >= 48 && keyCode <= 57))) )) // 0 ~ 9
+			  {
+			  	var keyCode, holdSelStart, holdSelEnd;
+			  	if ((keyCode === 190)||(keyCode === 194)) 
+			  		addKey = 46;
+			  	else if ((keyCode === 188)||(keyCode === 110))
+			  		addKey = 44;
+			  	else if (keyCode === 50 && shiftKey && this.acceptsMail)
+			  		addKey = 64;
+			  	else if (keyCode >= 65 && keyCode <= 90)
+			  		addKey = keyCode+32;
+			  	else if (keyCode >= 96 && keyCode <= 105)
+			  		addKey = keyCode-48;
+			  	else
+			  		addKey = keyCode;
+			  		
+				if (this.selectionEnd < this.selectionStart) {
+					holdSelStart = this.selectionEnd;
+					holdSelEnd = this.selectionStart;
+				} else {
+					holdSelStart = this.selectionStart;
+					holdSelEnd = this.selectionEnd;
+				}
+				this.text = this.text.substring(0, holdSelStart) + String.fromCharCode(addKey) + this.text.substring(holdSelEnd, this.text.length);
+				this.selectionStart = (this.selectionEnd = Math.min(holdSelStart + 1, this.text.length));
+				this.startTimer();
+				if (this.onChange instanceof Function)
+					this.onChange.call(this);
+				return true;
+			} else if (keyCode == 32)
+				return true;
+			//else if ([16,17,18,20].indexOf(keyCode) === -1) {
+				//console.log("Unhandled",keyCode);
+			//}
+		}
+		return false;
+	},
+	onMouseMove: function(x, y) {
+		if (this instanceof GuiInput) {
+			if ((this.focus)&&(this.box.checkBounds(x, y))&&(this.parent instanceof CanvasController)&&(this.parent.mouse.left)) {
+				var ctx = this.parent.ctx, leftNow = 0, i;
+				ctx.font = this.graphic.font;
+				for (i=0;i<this.text.length;i++)
+					if ((leftNow += ctx.measureText(this.text[i]).width)+this.graphic.margin > x-this.box.left+3)
+						break;
+				this.selectionEnd = i;
+				return true;
+			}
+		} else
+			console.error("Function must run from an instance of GuiButton");
+		return false;
 	},
 	onMouseUp: function (btnId, x, y) {
 		if (this instanceof GuiInput) {
-			if (btnId === 0) {
-				var isInside = this.box.checkBounds(x, y);
-				if (this.state === 2) {
-					if (isInside) {
-						this.isDown = false;
-						if (this.onClick instanceof Function)
-							this.onClick(x-this.box.left, y-this.box.top);
-					}
-				}
-				this.state = isInside?1:0;
+			if ((btnId === 0)&&(this.focus)&&(this.box.checkBounds(x, y))) {
+				var ctx = this.parent.ctx, leftNow = 0, i;
+				ctx.font = this.graphic.font;
+				for (i=0;i<this.text.length;i++)
+					if ((leftNow += ctx.measureText(this.text[i]).width)+this.graphic.margin > x-this.box.left+3)
+						break;
+				this.selectionEnd = i;
 			}
 		} else
 			console.error("Function must run from an instance of GuiButton");
 	},
+	update: function() {
+		if (this.focus) {
+			this.blinkState = !this.blinkState;
+			if (typeof(CanvasController) !== "undefined" && this.parent instanceof CanvasController) {
+				this.clear(this.parent.ctx);
+				this.draw(this.parent.ctx);
+			}
+		}
+	},
 	clear: function(ctx) {
-		ctx.clearRect(this.box.left-1, this.box.top-1, this.box.width+this.graphic.shadow+2, this.box.height+this.graphic.shadow+2);
+		if (this instanceof GuiInput)
+			if (ctx instanceof CanvasRenderingContext2D)
+				ctx.clearRect(this.box.left-2, this.box.top-2, this.box.width+4, this.box.height+4);
+			else 
+				console.error("First parameter is supposed to be instance of CanvasRenderingContext2D:",ctx);
+		else
+			console.error("Function must run from an instance of GuiInput");
 	},
 	draw: function(ctx) {
 		if (this instanceof GuiInput) {
 			if (ctx instanceof CanvasRenderingContext2D) {
 				var roundness = this.graphic.roundness;
+				var i, barBegin, barEnd, leftNow, holdSelStart, holdSelEnd;
 				ctx.fillStyle = this.graphic.bgColor;
-				ctx.strokeStyle = this.graphic.textColor;
+				if (this.focus) {
+					ctx.strokeStyle = this.graphic.focusColor;
+					ctx.lineWidth = this.graphic.focusBorder;
+				} else {
+					ctx.strokeStyle = '#333';
+					ctx.lineWidth = 1;
+				}
 				ctx.beginPath();
 				ctx.moveTo(this.box.left+roundness, this.box.top);
 				ctx.lineTo(this.box.right-roundness, this.box.top);
@@ -94,36 +370,59 @@ GuiInput.prototype = {
 				ctx.quadraticCurveTo(this.box.left, this.box.top, this.box.left+roundness, this.box.top);
 				ctx.stroke();
 				ctx.fill();
-				ctx.fillStyle = this.graphic.textColor;
 				
-				ctx.textAlign="left";
-				ctx.fillStyle = this.graphic.textColor;
-				ctx.font = this.graphic.font;
-				if (this.multiLine) {
-					ctx.textBaseline="top";
-					ctx.fillText(this.text, (this.box.left+this.box.right)/2, this.box.top+this.graphic.marginTop);
-				} else {
-					ctx.textBaseline="middle";
-					ctx.fillText(this.text, (this.box.left+this.box.right)/2, (this.box.top+this.box.bottom)/2);
+				ctx.strokeStyle = this.graphic.textColor;
+				if ((this.focus)&&((this.graphic.blinks&&this.blinkState)||(this.selectionStart != this.selectionEnd))) {
+					if (this.selectionEnd < this.selectionStart) {
+						holdSelStart = this.selectionEnd;
+						holdSelEnd = this.selectionStart;
+					} else {
+						holdSelStart = this.selectionStart;
+						holdSelEnd = this.selectionEnd;
+					}
+					barBegin = 0;
+					barEnd = 0;
+					leftNow = 0;
+					for (i=0;i<=this.text.length;i++) {
+						if (holdSelStart === i)
+							barBegin = leftNow;
+						if (holdSelEnd === i) {
+							barEnd = leftNow;
+							break;
+						}
+						if (i < this.text.length)
+							leftNow += ctx.measureText(this.text[i]).width;
+					}
+					ctx.beginPath();
+					if (this.selectionEnd < this.selectionStart) {
+						ctx.moveTo(this.box.left+this.graphic.margin+barBegin, this.box.top+this.graphic.margin);
+						ctx.lineTo(this.box.left+this.graphic.margin+barBegin, this.box.bottom-this.graphic.margin);
+					} else {
+						ctx.moveTo(this.box.left+this.graphic.margin+barEnd, this.box.top+this.graphic.margin);
+						ctx.lineTo(this.box.left+this.graphic.margin+barEnd, this.box.bottom-this.graphic.margin);
+					}
+					ctx.stroke();
+					
+					if (barBegin !== barEnd) {
+						ctx.fillStyle = "rgba(221,221,221,0.3)";
+						ctx.fillRect(this.box.left+this.graphic.margin+barBegin,this.box.top+this.graphic.margin*1.1,barEnd-barBegin,this.box.height-this.graphic.margin*2.2);
+					}
 				}
-				ctx.restore();
+				
+				ctx.fillStyle = this.graphic.textColor;
+				ctx.textAlign="left";
+				ctx.font = this.graphic.font;
+				ctx.textBaseline="middle";
+				ctx.fillText(this.text, this.box.left+this.graphic.margin, (this.box.top+this.box.bottom)/2);
+				
 			} else 
 				console.error("First parameter is supposed to be instance of CanvasRenderingContext2D:",ctx);
 		} else
 			console.error("Function must run from an instance of GuiInput");
 	},
-	autosize: function(ctx) {
-		if (this instanceof GuiInput) {
-			if (ctx instanceof CanvasRenderingContext2D)
-				return (this.box.width = this.graphic.margin.left+this.graphic.margin.right+ctx.measureText(this.text).width);
-			else
-				return (this.box.width = this.graphic.margin.left+this.graphic.margin.right+text.length*4.71); // magic number = approximation
-		} else
-			console.error("Function must run from an instance of GuiInput");
-	},
 	checkBounds: function(x, y) {
 		if (this instanceof GuiInput) {
-			return ((x > this.box.left) && (x < this.box.right+this.graphic.shadow) && (y > this.box.top) && (y < this.box.bottom+this.graphic.shadow)); 
+			return this.box.checkBounds.call(this.box,x,y); 
 		} else
 			console.error("Function must run from an instance of GuiInput");
 	}
