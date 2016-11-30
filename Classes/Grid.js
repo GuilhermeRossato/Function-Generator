bddc314cd9cef6dc2ad7e036e4a5e7c50fb99e20
Guilherme Rossato <guilhermrossato@gmail.com>
2016-11-30 02:27:27 -0200

    Button support for Grid
    Increase/decrease grid division. Also the possibility of not using the
    grid.

--- Classes/Grid.js
+++ Classes/Grid.js
@@ -1,12 +1,15 @@
 function Grid(cnvc) {
 	this.box = new GuiBox(0,0,cnvc.width,cnvc.height);
+	var ret = getCookie("divisions");
+	if (ret)
+		this.graphic.divisions = parseInt(ret);
 }
 Grid.prototype = {
 	constructor: Grid,
 	graphic: {
-		marginPercent: 0.13,
+		marginPercent: 0.11,
 		marginPx: 0,
-		divisions: 8,
+		divisions: 16,
 		exceedPercent: 0,
 		exceedPx: 9,
 		borderDash: [],
@@ -40,32 +43,34 @@ Grid.prototype = {
 		ctx.restore();
 	},
 	drawDivisions: function(ctx) {
-		ctx.save();
-		ctx.strokeStyle = "#ffffff";
-		ctx.setLineDash(this.graphic.gridDash);
-		ctx.lineWidth = 0.5;
-		ctx.beginPath();
-		var step = (1 - this.graphic.marginPercent * 2) / this.graphic.divisions;
-		for (var i = 0; i <= this.graphic.divisions; i++) {
-			// Line tips (exceedPx and exceedPercent)
-			ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent - this.graphic.exceedPercent) - this.graphic.exceedPx | 0) + 0.5);
-			ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent) | 0) + 0.5);
-			ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, 1 - this.graphic.marginPercent + this.graphic.exceedPercent) + this.graphic.exceedPx | 0) + 0.5);
-			ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, 1 - this.graphic.marginPercent) | 0) + 0.5);
-			ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent - this.graphic.exceedPercent) - this.graphic.exceedPx | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
-			ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
-			ctx.moveTo((b(this.box.left, this.box.right, 1 - this.graphic.marginPercent + this.graphic.exceedPercent) + this.graphic.exceedPx | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
-			ctx.lineTo((b(this.box.left, this.box.right, 1 - this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
-			// Middle Lines
-			if (i !== 0 && i !== this.graphic.divisions) {
-				ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent) | 0) + 0.5);
+		if (this.graphic.divisions != -1) {
+			ctx.save();
+			ctx.strokeStyle = "#ffffff";
+			ctx.setLineDash(this.graphic.gridDash);
+			ctx.lineWidth = 0.5;
+			ctx.beginPath();
+			var step = (1 - this.graphic.marginPercent * 2) / this.graphic.divisions;
+			for (var i = 0; i <= this.graphic.divisions; i++) {
+				// Line tips (exceedPx and exceedPercent)
+				ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent - this.graphic.exceedPercent) - this.graphic.exceedPx | 0) + 0.5);
+				ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent) | 0) + 0.5);
+				ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, 1 - this.graphic.marginPercent + this.graphic.exceedPercent) + this.graphic.exceedPx | 0) + 0.5);
 				ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, 1 - this.graphic.marginPercent) | 0) + 0.5);
-				ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
+				ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent - this.graphic.exceedPercent) - this.graphic.exceedPx | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
+				ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
+				ctx.moveTo((b(this.box.left, this.box.right, 1 - this.graphic.marginPercent + this.graphic.exceedPercent) + this.graphic.exceedPx | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
 				ctx.lineTo((b(this.box.left, this.box.right, 1 - this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
+				// Middle Lines
+				if (i !== 0 && i !== this.graphic.divisions) {
+					ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent) | 0) + 0.5);
+					ctx.lineTo((b(this.box.left, this.box.right, this.graphic.marginPercent + step * i) | 0) + 0.5, (b(this.box.top, this.box.bottom, 1 - this.graphic.marginPercent) | 0) + 0.5);
+					ctx.moveTo((b(this.box.left, this.box.right, this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
+					ctx.lineTo((b(this.box.left, this.box.right, 1 - this.graphic.marginPercent) | 0) + 0.5, (b(this.box.top, this.box.bottom, this.graphic.marginPercent + step * i) | 0) + 0.5);
+				}
 			}
+			ctx.stroke();
+			ctx.restore();
 		}
-		ctx.stroke();
-		ctx.restore();
 	},
 	drawText: function(ctx) {
 		ctx.save();
@@ -81,6 +86,36 @@ Grid.prototype = {
 		this.drawBox(ctx);
 		this.drawDivisions(ctx);
 		this.drawText(ctx)
+	},
+	canIncreaseGridDivision: function() {
+		return (this.graphic.divisions != -1);
+	},
+	canDecreaseGridDivision: function() {
+		return (this.graphic.divisions > 3) || (this.graphic.divisions == -1);
+	},
+	// Returns true if sucessful
+	increaseGridDivisions: function() {
+		if ((this.graphic.divisions != -1) && (this.graphic.divisions < 20)) {
+			this.graphic.divisions++;
+		} else if (this.graphic.divisions == -1) {
+			return false;
+		} else {
+			this.graphic.divisions = -1;
+		}
+		setCookie("divisions", this.graphic.divisions);
+		return true;
+	},
+	decreaseGridDivisions: function() {
+		if (this.graphic.divisions == -1) {
+			this.graphic.divisions = 20;
+		} else if (this.graphic.divisions > 4) {
+			this.graphic.divisions--;
+		} else if (this.graphic.divisions != 3) {
+			this.graphic.divisions = 3;
+		} else
+			return false;
+		setCookie("divisions", this.graphic.divisions);
+		return true;
 	},
 	resize: function(width, height) {
 		this.box.width = width;
